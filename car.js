50d5fbd1d97c5e42e5909053bce9aa78fc0e5786
muhammadisarwar <miserwar@gmail.com>
2016-12-06 15:04:35 +0500

    Create car.js

---
+++ car.js
@@ -0,0 +1,11 @@
+class Car extends Auto {
+    constructor(data) {
+        super(data);
+        this.isElectric = data.isElectric;
+        this.isHatchback = data.isHatchback;
+    }
+
+    getDetails() {
+        return `${super.getDetails()} Electric: ${this.isElectric} Hatchback: ${this.isHatchback}`;
+    }
+}
